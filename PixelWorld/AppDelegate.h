9ea4c1b6bfaf704127019e86f04ea9cb89a0d850
YifangZhang <yifangzhang2009@gmail.com>
2015-12-13 11:47:24 -0600

    Initial Commit

---
+++ PixelWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PixelWorld
+//
+//  Created by Yifang Zhang on 12/13/15.
+//  Copyright © 2015 Yifang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PixelWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PixelWorld
+//
+//  Created by Yifang Zhang on 12/13/15.
+//  Copyright © 2015 Yifang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
